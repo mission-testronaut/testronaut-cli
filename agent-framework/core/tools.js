a573de902ad3fdd68f78f20f3671c477dccc136e
shaneinceptionu <shane@inceptionu.com>
2025-08-03 01:52:57 -0600

    file purge clean up

--- agent-framework/core/tools.js
+++
@@ -1,9 +0,0 @@
-import { Browser } from './browser.js';
-
-export const TOOL_MAP = {
-  navigate: (browser, args) => browser.navigate(args),
-  fill: (browser, args) => browser.fill(args),
-  click: (browser, args) => browser.click(args),
-  get_dom: (browser, args) => browser.get_dom(args),
-  check_text: (browser, args) => browser.check_text(args),
-};
--- agent-framework/index.js
+++
@@ -1,6 +0,0 @@
-import { runWorkflow } from './runner/agentRunner.js';
-import { loginWorkflow } from './workflows/loginWorkflow.js';
-
-(async () => {
-  await runWorkflow(loginWorkflow);
-})();
